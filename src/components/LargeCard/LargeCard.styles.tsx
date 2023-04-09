a1811790c64d5fead17ec11cbcb5fe8dc40daa3c
Niels Louwes <niels.louwes@and.digital>
2023-04-09 12:22:42 +0200

    added image and some styling largecard

--- src/components/LargeCard/LargeCard.styles.tsx
+++ src/components/LargeCard/LargeCard.styles.tsx
@@ -1,15 +1,39 @@
 import styled from "styled-components";
 
-export const Card = styled.div``
+export const Card = styled.div`
+  height: 500px;
+  width: 300px;
+`;
 
-export const CardTop = styled.div``
+export const CardTop = styled.div`
+  height: 350px;
+  background: hsl(246, 80%, 60%);
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-export const ImageDiv = styled.div``
+export const ImageDiv = styled.div`
+  margin-bottom: 50px;
+`;
 
-export const ReportForText = styled.p``
+export const ReportForText = styled.p`
+  color: lightgrey;
+`;
 
-export const Name = styled.h2``
+export const Name = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  color: white;
+`;
 
-export const CardBottom = styled.div``
+export const CardBottom = styled.div`
+  height: 150px;
+  background-color: hsl(235, 46%, 20%);
+  color: white;
+`;
 
-export const TimePeriod = styled.div``
+export const TimePeriod = styled.div`
+  margin-bottom: 15px;
+`;
--- src/components/LargeCard/LargeCard.tsx
+++ src/components/LargeCard/LargeCard.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { Card, CardBottom, CardTop, Name, ReportForText, TimePeriod, ImageDiv } from "./LargeCard.styles";
+import {
+  Card,
+  CardBottom,
+  CardTop,
+  Name,
+  ReportForText,
+  TimePeriod,
+  ImageDiv,
+} from "./LargeCard.styles";
+import Image from "next/image";
 
 export default function LargeCard() {
   return (
     <>
       <Card>
         <CardTop>
-            <ImageDiv></ImageDiv>
+          <ImageDiv>
+            <Image
+              src="/image-jeremy.png"
+              alt="My Image"
+              width={75}
+              height={75}
+            />
+          </ImageDiv>
+          <div>
             <ReportForText>Report for</ReportForText>
             <Name>Jeremy Robson</Name>
+          </div>
         </CardTop>
         <CardBottom>
           <TimePeriod>Daily</TimePeriod>
